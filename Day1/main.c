6c87264647c9ced6fe87cc51364b130613dd5923
Brian Lee <hlee5@stuy.edu>
2018-12-04 23:19:14 -0500

    Temporarily breaking D1P2

--- Day1/main.c
+++ Day1/main.c
@@ -8,43 +8,14 @@ int main()
     int freq = 0;
     FILE *input = fopen("input", "r");
     char buf[256];
+    int freqs[1100];
+    int len = 0;
     while (fgets(buf, 256, input))
     {
         freq += atoi(buf);
+        freqs[len++] = freq;
     }
     printf("Part 1 Answer:\n%d\n", freq);
 
-    /*
-     * Reset for part 2
-     */
-    rewind(input);
-    freq = 0;
-    int hist_size = 256; 
-    int *freq_history = calloc(hist_size, sizeof(int));
-    int freq_index = 0;
-    int i;
-    while (1)
-    {
-        if (fgets(buf, 256, input))
-        {
-            freq += atoi(buf);
-            for (i = 0; i < freq_index; i++)
-            {
-                if (freq_history[i] == freq)
-                {
-                    printf("Part 2 Answer:\n%d\n", freq);
-                    return 0;
-                }
-            }
-            freq_history[freq_index++] = freq;
-            if (freq_index >= hist_size)
-            {
-                hist_size *= 2;
-                freq_history = realloc(freq_history, hist_size * sizeof(int));
-            }
-        }
-        else
-            rewind(input);
-    }
-    return 0;
+    // TODO: Improved part2
 }
