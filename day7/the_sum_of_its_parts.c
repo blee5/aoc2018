bee078accd63d406418abcac9f78a9bf0158ad67
Brian Lee <hlee5@stuy.edu>
2018-12-07 10:48:54 -0500

    Day 7 Part 1

--- day7/the_sum_of_its_parts.c
+++ day7/the_sum_of_its_parts.c
@@ -5,25 +5,96 @@
 
 struct step
 {
-    char status;
     char name;
+    char is_done;
     struct step **prereq_for;
+    struct step **prereq;
 };
 
-struct step steps[26];
+struct step *steps[26];
+struct step **available;
+
+void append(struct step **list, struct step *s)
+{
+    int i;
+    for (i = 0; list[i]; i++);
+    list[i] = s;
+}
+
+void del(struct step **list, struct step *s)
+{
+    int i = 0;
+    while (list[i++] != s);
+    i--;
+    for (; i < 25; i++)
+    {
+        list[i] = list[i + 1];
+    }
+}
+
+void add_step(char name)
+{
+    int index = name - 'A';
+    if (steps[index] == NULL)
+    {
+        steps[index] = malloc(sizeof(struct step));
+        steps[index]->name = name;
+        steps[index]->is_done = 0;
+        steps[index]->prereq_for = calloc(26, sizeof(struct step *));
+        steps[index]->prereq = calloc(26, sizeof(struct step *));
+    }
+}
 
 void add_dep(char name, char dep)
 {
-    int i;
-    struct step *s = &steps[dep - 'A'];
-    for (i = 0; s->prereq_for[i] != NULL; i++);
-    s->prereq_for[i] = &steps[name - 'A'];
+    append(steps[dep - 'A']->prereq_for, steps[name - 'A']);
+    append(steps[name - 'A']->prereq, steps[dep - 'A']);
 }
 
-void run_step(struct step *s)
+struct step *get_first()
+{
+    struct step *first = NULL;
+    int i;
+    for (i = 0; available[i]; i++)
+    {
+        if (!first || available[i]->name < first->name)
+        {
+            first = available[i];
+        }
+    }
+    return first;
+}
+
+int prereq_met(struct step *s)
 {
     int i;
-    printf("%c", s->name);
+    for (i = 0; s->prereq[i]; i++)
+    {
+        if (s->prereq[i]->is_done == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void run()
+{
+    while (available[0] != NULL)
+    {
+        struct step *s = get_first();
+        int i;
+        s->is_done = 1;
+        for (i = 0; s->prereq_for[i]; i++)
+        {
+            if (prereq_met(s->prereq_for[i]))
+            {
+                append(available, s->prereq_for[i]);
+            }
+        }
+        del(available, s);
+        printf("%c", s->name);
+    }
 }
 
 int main()
@@ -35,33 +106,29 @@ int main()
         return -1;
     }
     char buf[256];
-    int i;
-    for (i = 0; i < 26; i++)
-    {
-        steps[i].name = 'A' + i;
-        steps[i].status = -1;
-        steps[i].prereq_for = calloc(sizeof(struct step *), 10);
-    }
+    available = calloc(26, sizeof(struct step **));
+    memset(available, 0, 10 * sizeof(struct step **));
+    memset(steps, 0, 26 * sizeof(struct step **));
     while (fgets(buf, 256, input))
     {
         // Read file
         char name, depends;
         sscanf(buf, "Step %c must be finished before step %c can begin.", &depends, &name);
-        steps[name - 'A'].status = 0;
+        add_step(name);
+        add_step(depends);
         add_dep(name, depends);
     }
 
-    struct step *start;
+    int i;
     for (i = 0; i < 26; i++)
     {
-        if (steps[i].status != -1 && steps[i].prereq_for[0] == NULL)
+        if (steps[i] && steps[i]->prereq[0] == NULL)
         {
-            start = &steps[i];
-            break;
+            append(available, steps[i]);
         }
     }
     printf("Part 1 Answer:\n");
-    run_step(start);
+    run();
     printf("\n");
 
     /* printf("Part 2 Answer:\n%d\n", -1); */
