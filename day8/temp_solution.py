755cdde5778e9a87c8cb5d39d54fd6f14796d036
Brian Lee <hlee5@stuy.edu>
2018-12-08 01:24:06 -0500

    eh

--- day8/temp_solution.py
+++ day8/temp_solution.py
@@ -1,38 +1,9 @@
 with open("input", 'r') as f:
     data = map(int, f.read().split())
 
-def read_node(i):
-    metadata_sum = 0
-    children_length = 0
-
-    # Read header
-    num_child = data[i] 
-    num_metadata = data[i + 1]
-    i += 2
-
-    # Get metadata sums from children
-    for j in range(num_child):
-        temp = part2(i + children_length)
-        children_length += temp[0]
-        meta_sum += temp[1]
-
-    while (num_child > 0):
-        temp = read_node(i + children_size)
-        children_size += temp[0]
-        meta_sum += temp[1]
-        num_child -= 1
-
-    # Shift index by size of child nodes
-    i += children_size
-    for j in range(num_metadata):
-        metadata_sum += data[i]
-        i += 1
-
-    return 2 + children_length + num_metadata, metadata_sum
-
 def checksum(i):
-    metadata_sum1 = 0 # part1 
-    metadata_sum2 = 0 # part2
+    total = 0 # part1 
+    value = 0 # part2
     children_length = 0
 
     # Read header
@@ -45,20 +16,20 @@ def checksum(i):
     for j in range(num_child):
         temp = checksum(i + children_length)
         children_length += temp[0]
-        metadata_sum1 += temp[1]
+        total += temp[1]
         child_sums.append(temp[2])
 
     # Read metadata
     i += children_length
     for j in range(num_metadata):
-        metadata_sum1 += data[i]
+        total += data[i]
         if (num_child == 0):
-            metadata_sum2 += data[i]
+            value += data[i]
         elif (data[i] - 1 < num_child):
-            metadata_sum2 += child_sums[data[i] - 1]
+            value += child_sums[data[i] - 1]
         i += 1
 
-    return 2 + children_length + num_metadata, metadata_sum1, metadata_sum2
+    return 2 + children_length + num_metadata, total, value
 
 sums = checksum(0)
 print("Part 1 Answer:\n" + str(sums[1]))
