e8d5b05fe1d9ae633811c4ec3ac182c23c69965d
Brian Lee <hlee5@stuy.edu>
2018-12-10 15:12:42 -0500

    Day 8 solution in C

--- day08/memory_maneuver.c
+++ day08/memory_maneuver.c
@@ -1,21 +1,57 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+int *data;
+
+void read_node(int *i, int *total, int *value)
+{
+    int child_count = data[(*i)++];
+    int meta_count = data[(*i)++];
+
+    int *child_values = calloc(child_count, sizeof(int));
+    int j;
+    for (j = 0; j < child_count; j++)
+    {
+        read_node(i, total, &child_values[j]);
+    }
+
+    for (j = 0; j < meta_count; j++)
+    {
+        *total += data[*i];
+        if (child_count == 0)
+            *value += data[*i];
+        else
+        {
+            int child_idx = data[*i] - 1;
+            if (child_idx < child_count)
+                *value += child_values[child_idx];
+        }
+        (*i)++;
+    }
+}
 
 int main()
 {
-    FILE *input = fopen("input2", "r");
+    FILE *input = fopen("input", "r");
     if (input == NULL)
     {
         perror("No input file found");
         return -1;
     }
-    char buf[256];
-    while (fgets(buf, 256, input))
-    {
-        // Read file
-    }
+    char *buf = malloc(40000);
+    fgets(buf, 40000, input);
 
-    printf("Part 1 Answer:\n%d\n", -1);
-    printf("Part 2 Answer:\n%d\n", -1);
+    int i = 0;
+    data = malloc(40000 * sizeof(int));
+    char *tok;
+    while (tok = strsep(&buf, " "))
+        data[i++] = atoi(tok);
+
+    int p1 = 0, p2 = 0;
+    i = 0;
+    read_node(&i, &p1, &p2);
+    printf("Part 1 Answer:\n%d\n", p1);
+    printf("Part 2 Answer:\n%d\n", p2);
 }
